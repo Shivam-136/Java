a6b48528bd8c5898d485cba1703dbf3c524feba8
Shivam... <shivam243028@gmail.com>
2026-07-10 21:42:28 +0530

    Maximum Number

---
+++ pts-Question/Maximum.java
@@ -0,0 +1,15 @@
+public class Maximum {
+
+    static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(15, 25));
+    }
+}
