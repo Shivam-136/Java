08ea1fc67a91ed52f8bd9a09682237903b6eaca0
Shivam... <shivam243028@gmail.com>
2026-07-13 18:33:15 +0530

    Power

--- pts-Question/Power.java
+++ pts-Question/Power.java
@@ -1,3 +1,17 @@
+// Write a function to calculate a^b.
+
 public class Power {
-    
+
+    static int power(int a, int b) {
+
+        int result = 1;
+
+        for(int i=1; i<=b; i++){
+         result *= a;
+        }
+    return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(power(2,5));
+    }
 }
