ac65b026afc616213d980f58235fdcc46e44055d
Shivam... <shivam243028@gmail.com>
2026-07-13 18:36:46 +0530

    Square

--- pts-Question/Square.java
+++ pts-Question/Square.java
@@ -1,3 +1,10 @@
+// Write a function to calculate the square of a number.
+
 public class Square {
-    
+    static int square(int n){
+        return n*n;
+    }
+    public static void main(String[] args) {
+        System.out.println(square(8));
+    }
 }
