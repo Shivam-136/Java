49592b492217bcd16db910c96a5d64709e36d8c5
Shivam... <shivam243028@gmail.com>
2026-07-20 22:04:36 +0530

    checkLeapYear

--- Function/LeapYear.java
+++ Function/LeapYear.java
@@ -1,14 +1,14 @@
 public class LeapYear {
-    
-    static void CheckLeapYear(int year){
 
-        if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+    static void checkLeapYear(int year) {
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
             System.out.println("Leap Year");
         else
             System.out.println("Not a Leap Year");
     }
 
     public static void main(String[] args) {
-        CheckLeapYear(2020);
+        checkLeapYear(2024);
     }
 }
