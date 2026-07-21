34fa30b652821505f1297981f143a6111ad2bee0
Shivam... <shivam243028@gmail.com>
2026-07-21 22:55:42 +0530

    RemoveSpaces

---
+++ Function/RemoveSpaces.java
@@ -0,0 +1,12 @@
+public class RemoveSpaces {
+
+    static String removeSpaces(String str) {
+
+        return str.replace(" ", "");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(removeSpaces("Java    Programming"));
+    }
+}
