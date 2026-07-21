23de6c78a38184c6fa7f4e4fdfcb77bae5c0f736
Shivam... <shivam243028@gmail.com>
2026-07-21 23:11:50 +0530

    UpperCase

--- Function/UpperCase.java
+++ Function/UpperCase.java
@@ -1,9 +1,8 @@
 public class UpperCase {
-    static String convert(String str){
 
+    static String convert(String str){
         return str.toUpperCase();
     }
-
     public static void main(String[] args) {
         System.out.println(convert("java programming"));
     }
