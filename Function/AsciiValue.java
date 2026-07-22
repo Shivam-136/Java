d22dcf00df872692bbb96ea5ecc974a0f5f3a999
Shivam... <shivam243028@gmail.com>
2026-07-22 23:34:06 +0530

    AsciiValue

---
+++ Function/AsciiValue.java
@@ -0,0 +1,9 @@
+public class AsciiValue {
+    
+    static int ascii(char ch){
+        return ch;
+    }
+    public static void main(String[] args) {
+        System.out.println(ascii('A'));
+    }
+}
