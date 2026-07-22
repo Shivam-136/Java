a8f1717a46e99069fffbcf43e6cfb36633d9dd69
Shivam... <shivam243028@gmail.com>
2026-07-22 22:46:14 +0530

    LowerCase

---
+++ Function/LowerCase.java
@@ -0,0 +1,11 @@
+public class LowerCase{
+
+    static String convert(String str){
+
+        return str.toLowerCase();
+    }
+    public static void main(String[] args) {
+        
+        System.err.println(convert("JAVA"));
+    }
+}
